dbd73d9b9157d03abf16fd905001acdf4eb9307a
luckyantony <luckyantonyofficial@gmail.com>
2025-04-14 02:11:11 +0300

    making changes in the app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,34 @@
-import { useState } from 'react'
-import './App.css'
-import Header from './components/Header'
+import { useState } from 'react';
+import Header from './components/Header';
+import AddExpenseForm from './components/AddExpenseForm';
 import ExpensesTable from './components/ExpensesTable';
+import SearchBar from './components/SearchBar';
 
 function App() {
   const [expenses, setExpenses] = useState([
     { id: 1, name: 'Groceries', description: 'Supermarket', amount: 200, date: '2024-04-01' },
     { id: 2, name: 'Rent', description: 'April Rent', amount: 1000, date: '2024-04-02' }
   ]);
+
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const handleAddExpense = (newExpense) => {
+    setExpenses([...expenses, newExpense]);
+  };
+
+  const handleSearch = (term) => {
+    setSearchTerm(term);
+  };
+
+  
+
   return (
-    <>
+    <div className="App">
       <Header />
-      <ExpensesTable />
-    </>
-  )
+      <SearchBar searchTerm={searchTerm} onSearch={handleSearch} />
+      <AddExpenseForm onAddExpense={handleAddExpense} />
+    </div>
+  );
 }
 
-export default App
+export default App;
