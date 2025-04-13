7cfd7e74084eb251f28382b7d0c70436e318b800
luckyantony <luckyantonyofficial@gmail.com>
2025-04-14 01:25:09 +0300

    Adding expense tab

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import { useState } from 'react'
 import './App.css'
 import Header from './components/Header'
+import ExpensesTable from './components/ExpensesTable';
 
 function App() {
-
+  const [expenses, setExpenses] = useState([
+    { id: 1, name: 'Groceries', description: 'Supermarket', amount: 200, date: '2024-04-01' },
+    { id: 2, name: 'Rent', description: 'April Rent', amount: 1000, date: '2024-04-02' }
+  ]);
   return (
     <>
       <Header />
+      <ExpensesTable />
     </>
   )
 }
---
+++ src/components/ExpensesTable.jsx
@@ -0,0 +1,28 @@
+function ExpensesTable({ expenses, }) {
+    return (
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Description</th>
+            <th>Amount</th>
+            <th>Date</th>
+            <th>Delete</th>
+          </tr>
+        </thead>
+        <tbody>
+          {expenses.map(exp => (
+            <tr key={exp.id}>
+              <td>{exp.name}</td>
+              <td>{exp.description}</td>
+              <td>${exp.amount}</td>
+              <td>{exp.date}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    );
+  }
+  
+  export default ExpensesTable;
+  
