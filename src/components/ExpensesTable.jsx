69e0b0ef33d4f4b327af9bf88148e3ba04138871
luckyantony <luckyantonyofficial@gmail.com>
2025-04-15 03:24:18 +0300

    Adding the delete button

--- src/components/ExpensesTable.jsx
+++ src/components/ExpensesTable.jsx
@@ -1,28 +1,28 @@
-function ExpensesTable({ expenses, }) {
-    return (
-      <table>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Description</th>
-            <th>Amount</th>
-            <th>Date</th>
-            <th>Delete</th>
+function ExpensesTable({ expenses, onDelete }) {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Description</th>
+          <th>Amount</th>
+          <th>Date</th>
+          <th>Delete</th>
+        </tr>
+      </thead>
+      <tbody>
+        {expenses.map(exp => (
+          <tr key={exp.id}>
+            <td>{exp.name}</td>
+            <td>{exp.description}</td>
+            <td>${exp.amount}</td>
+            <td>{exp.date}</td>
+            <td><button onClick={() => onDelete(exp.id)}>Delete</button></td>
           </tr>
-        </thead>
-        <tbody>
-          {expenses.map(exp => (
-            <tr key={exp.id}>
-              <td>{exp.name}</td>
-              <td>{exp.description}</td>
-              <td>${exp.amount}</td>
-              <td>{exp.date}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    );
-  }
-  
-  export default ExpensesTable;
-  
+        ))}
+      </tbody>
+    </table>
+  );
+}
+
+export default ExpensesTable;
